4409475ad06884ad0121b49b71b6db751719140a
yiiamee <mykhailo.kukharskyi@right.link>
2023-10-05 11:52:41 +0300

    nuxt config changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'QR.QUEST',
+    },
+  },
+
+  devtools: { enabled: true },
+  ssr: true,
 })
